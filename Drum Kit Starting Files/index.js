5d6240b991400c802afbacf0336641df98e37c8d
Anshul koshta <ANSHULKOSHTA621@GMAIL.COM>
2025-01-15 10:32:26 +0530

    Add files via upload

---
+++ Drum Kit Starting Files/index.js
@@ -0,0 +1,13 @@
+var numberofdrumesbuttons = document.querySelectorAll(".drum").lenght;
+
+for (var i=0; i<numberofdrumesbuttons; i++)  {
+
+document.querySelectorAll(".drum")[i].addeventlistener("click", function() {alert('i got clicked');
+
+
+
+
+
+});
+
+}
