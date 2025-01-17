665616daeca92e7ebac869fbd5bc579dd6ca5b0a
Anshul koshta <ANSHULKOSHTA621@GMAIL.COM>
2025-01-17 12:28:06 +0530

    Update index.js

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -2,7 +2,7 @@ var numberofdrumesbuttons = document.querySelectorAll(".drum").lenght;
 
 for (var i=0; i<numberofdrumesbuttons; i++)  {
 
-document.querySelectorAll(".drum")[i].addeventlistener("click", function(){ var audio = new Audio('sound/drum');
+document.querySelectorAll(".drum")[i].addeventlistener("click", function(){ var audio = new Audio('sounds/crash.mp3');
 audio.play();
 
 
