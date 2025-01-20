1988162ba6034fa9a490cecb4975a84c17d48321
Anshul koshta <ANSHULKOSHTA621@GMAIL.COM>
2025-01-20 11:10:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,46 +1,54 @@
+
+
 var numberOfDrumButtons=document.querySelectorAll(".drum").length;
 for (var i = 0; i<numberOfDrumButtons; i++){
-     document.querySelectorAll(".drum")[i].addEventListener("click", function(){
+     document.querySelectorAll(".drum")[i].addEventListener("click", function() {
           var buttoninnerHTML = this.innerHTML;
+          makesound(buttoninnerHTML);
 
-          switch(buttoninnerHTML){
-                case "w" :
-               var tom1 = new Audio("sounds/tom-1.mp3");
-               tom1.play();
-               break;
-               case "a" :
-                    var tom2 = new Audio("sounds/tom-2.mp3");
-                    tom2.play();
-                    break;
+         
+     });
+ }
 
-               case "s" :
-                     var tom3 = new Audio("sounds/tom-3.mp3");
-                    tom3.play();
-                     break;
-               case "d" :
-                    var tom4 = new Audio("sounds/tom-4.mp3");
-                    tom4.play();
-                    break;
-               case "j" :
-                    var tom5 = new Audio("sounds/crash.mp3");
-                    tom5.play();
-                    break;
-                case "k" :
-                    var tom6 = new Audio("sounds/kick-bass.mp3");
-                    tom6.play();
-                    break;
+document.addEventListener("keypress", function(event){
+makesound(event.key);});
 
-               case "l" :
-                    var tom7 = new Audio("sounds/snare.mp3");
-                    tom7.play();
-                    break;
+//detectiing key board press
+ 
+function makesound(key){
 
-                default:    
-                                                              
-               
-                
-               
-          
-          
-          }
-}); }
+      switch(key){
+     case "w" :
+    var tom1 = new Audio("sounds/tom-1.mp3");
+    tom1.play();
+    break;
+    case "a" :
+         var tom2 = new Audio("sounds/tom-2.mp3");
+         tom2.play();
+         break;
+
+    case "s" :
+          var tom3 = new Audio("sounds/tom-3.mp3");
+         tom3.play();
+          break;
+    case "d" :
+         var tom4 = new Audio("sounds/tom-4.mp3");
+         tom4.play();
+         break;
+    case "j" :
+         var tom5 = new Audio("sounds/crash.mp3");
+         tom5.play();
+         break;
+     case "k" :
+         var tom6 = new Audio("sounds/kick-bass.mp3");
+         tom6.play();
+         break;
+
+    case "l" :
+         var tom7 = new Audio("sounds/snare.mp3");
+         tom7.play();
+         break;
+
+     default: console.log(buttoninnerHTML);    
+
+}}
